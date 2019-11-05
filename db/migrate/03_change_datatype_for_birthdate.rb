266a94f483ad8c81dcabf5733f80be6226d2282c
Steven Laff <steve.laff@gmail.com>
2019-11-05 04:08:18 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
     change_column :students do |t|
-      t.integer :grade
-      t.string :birthdate
+      
     end
   end
 end
